2cc94540f7d5c7d30a902c55912ef3815cae80dd
Stella Oh <50852653+Stella-J-Oh@users.noreply.github.com>
2024-11-19 11:58:53 -0500

    server stuff

--- Server.py
+++ Server.py
@@ -1,3 +1,22 @@
 class Server:
     def __init__(self):
             pass
+    
+# create a document database folder
+
+# when a user opens a document, an new instance of the server is created and kept open while the user is editing the document
+# once the user finishes editing the document/closes it, then the document is written to the database and the instance is closed
+    def open_doc():
+          return True
+    
+    def close_doc():
+          return True
+    
+    def edit_doc():
+            return True
+
+# add document to the database which is created by user
+# handle the case when 
+    def add_new_doc():
+         return True
+    
