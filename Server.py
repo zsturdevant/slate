bdd4a7e67f656f7080adda03c12b7830b3305845
Zach <zss1@williams.edu>
2024-11-21 11:48:17 -0500

    added stuff

--- Server.py
+++ Server.py
@@ -1,9 +1,71 @@
 import Socket.IO
+import os
 
 
 class Server:
-    def __init__(self):
-            pass
+
+    class Document:
+        def __init__(self, author_list, doc_name, contents =[]):
+            self.name = doc_name
+            self.author_list = author_list
+            self.contents = contents
+
+        def name_file(self, name):
+            self.doc_name = name
+            return 1
+
+
+        def add_author(self, new_author):
+            self.author_list.append(new_author)
+            return 1
+        
+        def remove_author(self, author_name):
+             if self.author_list.contains(author_name):
+                  #TODO
+                    ##remove it
+                    return 1
+             else:
+                  return 0
+
+        def update_contents(self, update_list, position_list):
+            if len(update_list) != len(position_list):
+                print("unable to update, list and positions were of different sizes")
+                return 0
+            
+            content_length = len(self.contents)
+
+            for i in range(len(update_list)):
+                update = update_list[i]
+                position = position_list[i]
+
+                if (position > content_length - 1):
+                    content_length += 1
+                    self.contents.append(update)
+                
+                else:
+                    self.contents[position] = update
+
+            return 1
+        
+        def save(self):
+
+            with open(self.doc_name, 'w') as file:
+                contents = self.contents.join(' ')
+                file.write(contents)
+
+            return 1
+
+    def __init__(self, document_list = []):
+         doc_path ="./documents"
+         entries = os.listdir(doc_path)
+         for entry in entries:
+              document_list.append(entry)
+
+    def add_doc(self, author, doc_name):
+         doc = Document([author], doc_name)
+
+          
+            
     
     # should we store a path to the database folder as a global variable?
     path_to_doc_database = "path?"
