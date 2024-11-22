80e91377c0a67031d319483063ca63331bd6edb8
Douglas Pineda Gutierrez <djp5@williams.edu>
2024-11-22 16:06:59 -0500

    more next

--- nextjs-slate/src/components/Header/index.tsx
+++ nextjs-slate/src/components/Header/index.tsx
@@ -1,8 +1,26 @@
 "use client";
 
+import { IconName } from "react-icons/fa6";
+
 export default function Header() {
     return (
-        <div className='flex justify-center text-2xl py-4'> Yuh </div>
+        <>
+        {/* <div className='flex justify-center text-2xl py-4'> Yuh </div> */}
+        <div className="header-wrapper flex flex-col w-full p-6">
+            <div className="top-part">
+                <input className="doc-name bg-[#191919]"/>
+                <div className="buttons">
+                    <button></button>
+                    <button></button>
+                </div>
+                <div className="menu"></div>
+            </div>
+            <div className="bottom-part bg-[#222222] text-[#5A5A5A]">
+                {/* formatting stuff like bold and sheet */}
+                Formatting bar
+            </div>
+        </div>
+        </>
     );
     
 }
