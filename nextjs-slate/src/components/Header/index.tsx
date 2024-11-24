ffe399f81608f9679d54ca453b539e49139b24db
Douglas Pineda Gutierrez <djp5@williams.edu>
2024-11-24 15:28:50 -0500

    icons and ui

--- nextjs-slate/src/components/Header/index.tsx
+++ nextjs-slate/src/components/Header/index.tsx
@@ -1,23 +1,25 @@
 "use client";
 
-import { IconName } from "react-icons/fa6";
+import { FaArrowLeftLong, FaArrowRightLong, FaBars } from "react-icons/fa6";
 
 export default function Header() {
     return (
         <>
         {/* <div className='flex justify-center text-2xl py-4'> Yuh </div> */}
         <div className="header-wrapper flex flex-col w-full p-6">
-            <div className="top-part">
-                <input className="doc-name bg-[#191919]"/>
-                <div className="buttons">
-                    <button></button>
-                    <button></button>
+            <div className="top-part flex items-center text-2xl gap-4 m-2">
+                <button className="menu"> <FaBars/> </button>
+                <div className="arrows flex items-center gap-2">
+                    <button> <FaArrowLeftLong/> </button>
+                    <button> <FaArrowRightLong/> </button>
                 </div>
-                <div className="menu"></div>
+                {/* TODO: Save name when eneter key is pressed and unfocus/unselect the input field*/}
+                <input className="doc-name placeholder:text-[#5A5A5A] bg-[#191919] w-auto" placeholder="Untitled Document"/>
+                
             </div>
-            <div className="bottom-part bg-[#222222] text-[#5A5A5A]">
+            <div className="bottom-part bg-[#222222] text-[#5A5A5A] rounded-md p-2">
                 {/* formatting stuff like bold and sheet */}
-                Formatting bar
+                Options bar
             </div>
         </div>
         </>
