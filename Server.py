2508814ad723219a26ab300c9dae4dd5329252a5
Zach <zss1@williams.edu>
2024-11-24 20:11:02 -0500

    server works (I think)

--- Server.py
+++ Server.py
@@ -1,8 +1,8 @@
-# import Socket.IO
 import os
-import concurrent.futures
+import asyncio
 from fastapi import FastAPI, WebSocket
 from pycrdt import Doc, Array
+import uvicorn
 
 class Document:
     def __init__(self, author_list = [], doc_name = "Untitled", contents =[]):
@@ -15,8 +15,6 @@ class Document:
         contents = Array()
         doc["array"] = contents
         self.doc = doc
-
-
 
     def name_file(self, name):
         self.name = name
@@ -41,27 +39,42 @@ class Document:
 
                  
     # handles updates to the crdt list 
-    def update_contents(self, operation_list, update_list, position_list):
-        if len(update_list) != len(position_list):
-            print("unable to update, list and positions were of different sizes")
+    def update_contents(self, operation, update, position):
+        if operation == "insert":
+            self.doc["array"].insert(position, update)
+
+        elif operation == "append":
+            self.doc["array"].append(update)
+
+        elif operation == "delete":
+            del self.doc["array"][position]
+
+        elif operation == "replace":
+            self.doc["array"][position] = update
+
+        else:
+            print("attempted to perform disallowed operation")
             return False
+        # if len(update_list) != len(position_list):
+        #     print("unable to update, list and positions were of different sizes")
+        #     return False
 
-        for i in range(len(update_list)):
-            operation = operation_list[i]
-            update = update_list[i]
-            position = position_list[i]
+        # for i in range(len(update_list)):
+        #     operation = operation_list[i]
+        #     update = update_list[i]
+        #     position = position_list[i]
 
-            if operation == "insert":
-                self.doc["array"].insert(position, update)
+        #     if operation == "insert":
+        #         self.doc["array"].insert(position, update)
 
-            elif operation == "append":
-                self.doc["array"].append(update)
+        #     elif operation == "append":
+        #         self.doc["array"].append(update)
 
-            elif operation == "delete":
-                del self.doc["array"][position]
+        #     elif operation == "delete":
+        #         del self.doc["array"][position]
 
-            elif operation == "replace":
-                self.doc["array"][position] = update
+        #     elif operation == "replace":
+        #         self.doc["array"][position] = update
 
         return True
     
@@ -78,14 +91,19 @@ class Document:
             return False
          
     
-    def save(self):
-        with open(self.name, 'w') as file:
+    def save(self, path):
+        with open(path + self.name, 'w') as file:
             contents = self.doc["array"].to_py()
             contents = ' '.join(contents)
             file.write(contents)
         return True
+    
+    def get_contents(self):
+        contents = self.doc["array"]
+        return ' '.join(contents)
 
-class Server:
+
+class File_Cabinet:
 
     def __init__(self, doc_path, document_list: list = []):
         self.doc_path = doc_path
@@ -99,8 +117,8 @@ class Server:
 
     # if the user creates a new document that doesn't already exist in the database, add it to the database
     def add_doc(self, author, doc_name):
-        full_doc_name = self.doc_path + doc_name
-        doc = Document([author], full_doc_name)
+        doc = Document([author], doc_name)
+
         if doc.name not in self.document_list:
             self.document_list.append(doc.name)
         return doc
@@ -108,7 +126,7 @@ class Server:
     # open an existing document
     def open_doc(self, author, doc_name):
          if doc_name in self.document_list:
-            path = doc_path + doc_name
+            path = self.doc_path + doc_name
             with open(path, 'r') as file:
                 contents = []
                 for next in file:
@@ -120,10 +138,10 @@ class Server:
               return self.add_doc(author, doc_name)
     
     # Allow user to delete a document if desired
-    def delete_doc(self, doc: Document, doc_path):
+    def delete_doc(self, doc: Document):
          if doc.name in self.document_list:
             self.document_list.remove(doc.name)
-         doc.delete(doc_path)
+         doc.delete(self.doc_path + doc.name)
          del doc
 
 
@@ -132,154 +150,141 @@ class Server:
         self.document_list.remove(doc.name)
         doc.name_file(new_name)
         self.document_list.append(new_name)
-         
-    
-    def open_connection():
-          return True
-    
-    # some functionality for keeping track of the user(s) who is on the doc
-    # do we implement some sort of authentication or login?
-    def user():
-          return True
-
-    
-    # utilize CRDTpy and array stuff for the live editing the user is doing on the doc
-    def edit_doc(doc: Document, operation_list, update_list, position_list):
-        return doc.update_contents(operation_list, update_list, position_list)
 
     # once the user finishes editing the document/closes it, write document to the database and close the instance
     def close_doc(self, doc: Document):
-          doc.save()
+          doc.save(self.doc_path)
           del doc
           return True
     
-    # connects to the client 
-    async def websocket_connection(websocket):
-        print("hello")
-
-
-    def handle_client(websocket: WebSocket):
-        print("hello")
-
+    #allow the user to save the file if they want to
+    def save_doc(self, doc):
+        doc.save(self.doc_path)
+        return True
     
 
-# app = FastAPI()
-
-# @app.websocket("/ws")
-# async def websocket_connection(server: Server, doc: Document, websocket: WebSocket):
-#     await websocket.accept()
-#     doc.add_author(websocket)
-#     try:
-#         keep_going = True
-#         while keep_going:
-#             command:str = await websocket.receive_text()
-
-#             # if the command contains edits to the document 
-#             if command.startswith("edit"):
-
-#                 #tell the other clients working on this document about the changes
-#                 for connection in doc.author_list:
-#                     if connection != websocket:
-#                         await connection.send_text(command)
-
-#                 # edit actual document 
-#                 lists = []
-#                 operation_list = []
-#                 update_list = []
-#                 position_list = []
-#                 lists.append(operation_list)
-#                 lists.append(update_list)
-#                 lists.append(position_list)
-#                 list_num = 0
-
-#                 raw_text = command.split(" ")
-#                 # need to figure out what the edits are 
-#                 for word in raw_text:
-#                     if word == "edit":
-#                         continue
-#                     elif word == "operation_list":
-#                         list_num = 0
-
-#                     elif word == "update_list":
-#                         list_num = 1
-
-#                     elif word == "position_list":
-#                         list_num = 2
-
-#                     else:
-#                         if list_num == 2:
-#                             lists[list_num].append(int(word))
-#                         else:
-#                             lists[list_num].append(word)
-
-#                 # update the contents of the document
-#                 doc.update_contents(operation_list, update_list, position_list)
-
-#             # close the connection with that user 
-#             elif command.startswith("close"): 
-#                 doc.author_list.remove(websocket)
-
-#                 #close the document fully if the last user is finished
-#                 if(doc.author_list.isEmpty()):
-#                     server.close_doc(doc)
-
-#                 keep_going = False
+# handle individual connections as they come
+async def handle_connection(websocket: WebSocket, server: File_Cabinet):
+    await websocket.accept()
+    try:
+        keep_going = True
+        doc:Document = None
+        while keep_going:
+            command:str = await websocket.receive_text()
+            breakdown = command.split(":")
             
 
+            # send the client a list of all current documents 
+            if breakdown[0] == "document_list":
+                docs = []
+                for name in server.document_list:
+                    docs.append(name)
+                text = ' '.join(docs)
+                await websocket.send_text(text)
+            
+            # open the file on the server and send the full file text to the client
+            elif breakdown[0] == "open":
+                doc = server.open_doc(breakdown[1])
+                text = doc.get_contents()
+                await websocket.send_text(text)
 
-#     except Exception as e:
-#         print(f"WebSocket connection closed: {e}")
+            elif breakdown[0] == "update":
+                if doc == None:
+                    print("invalid operation, attempted to update a document that has not been opened")
+                    continue
+
+                #tell the other clients working on this document about the changes
+                for connection in doc.author_list:
+                    if connection != websocket:
+                        await connection.send_text(command)
+
+                com = breakdown[1]
+                action = com.split(";")
+
+                if action[0] == "insert":
+                    pos_val = action[1]
+                    position, value = pos_val.split(' ')
+                    doc.update_contents(action[0], value, position)
+
+                elif action[0] == "append":
+                    value = action[1]
+                    doc.update_contents(action[0], value)
+
+                elif action[0] == "delete":
+                    position = action[1]
+                    doc.update_contents(position)
+
+                elif action[0] == "replace":
+                    pos_val = action[1]
+                    position, value = pos_val.split(' ')
+                    doc.update_contents(action[0], value, position)
+
+            # close the connection with that user 
+            elif breakdown[0] == "close": 
+                doc.author_list.remove(websocket)
+
+                #close the document fully if the last user is finished
+                if(doc.author_list.isEmpty()):
+                    server.close_doc(doc)
+
+                keep_going = False
+
+            elif breakdown[0] == "save":
+                server.save_doc(doc)
+
+            elif breakdown[0] == "delete":
+                server.delete_doc(doc)
+
+            elif breakdown[0] == "rename":
+                server.rename_doc(doc, breakdown[1])
+
+            else:
+                print("invalid operation")
+                await websocket.send_text(command + " was not a valid command")
 
 
-# The main method that runs the server
+    except Exception as e:
+        print(f"Error during WebSocket communication: {e}")
+        if websocket in doc.author_list:
+            doc.author_list.remove(websocket)
+
+app = FastAPI()
+
+# set the document path and initalize a server
+doc_path = "./documents/"
+server = File_Cabinet(doc_path)
+
+@app.websocket("/ws")
+async def websocket_connection(websocket: WebSocket):
+    # Hand off the connection to a new async task
+    asyncio.create_task(handle_connection(websocket, server))
+
+# the main method for running the server
 if __name__ == "__main__":
-    doc_path = "./documents/"
-    server = Server(doc_path, [])
-    server.add_doc("zach", "zach.txt")
-    doc = server.open_doc("zach", "zach.txt")
+    uvicorn.run("Server:app", host="127.0.0.1", port=8020, reload=True)
 
 
-    # server.delete_doc(doc, (doc_path + doc.name))
+# # The main method for testing server functionality
+# if __name__ == "__main__":
+#     doc_path = "./documents/"
+#     server = Server(doc_path, [])
+#     # doc = server.add_doc("zach", "zach.txt")
+#     doc = server.open_doc("zach", "zach.txt")
+#     # server.close_doc(doc)
 
 
-    operation_list = ["append", "append", "append", "delete", "insert"]
-    update_list = ["hello", "world", "my", "none", "silly"]
-    position_list = [0, 0, 0, 2, 1]
-    doc.update_contents(operation_list,update_list, position_list)
-
-    operation_list = ["append"]
-    update_list = ["hello"]
-    position_list = [0]
-    doc.update_contents(operation_list,update_list, position_list)
-    server.close_doc(doc)
-
-    
-
-    # make a pool of workers to handle different clients 
-    # thread_pool = concurrent.futures.ThreadPoolExecutor(max_workers= 10)
-
-    # server.close_doc(doc)
-
-    # keep_going = True
-    # while keep_going:
-    #     #wait for a websocket connection
-
-    #     # this needs to be finished
-    #     thread_pool.submit(websocket_connection, websocket)
-    #     keep_going = False
-
-    # thread_pool.shutdown(wait=True)
-    
-
-
+#     server.delete_doc(doc)
 
     
 
 
-          
-          
-    
-    
+#     # operation_list = ["append", "append", "append", "delete", "insert"]
+#     # update_list = ["hello", "world", "my", "none", "silly"]
+#     # position_list = [0, 0, 0, 2, 1]
+#     # doc.update_contents(operation_list,update_list, position_list)
 
-
-    
+#     # operation_list = ["append"]
+#     # update_list = ["hello"]
+#     # position_list = [0]
+#     # doc.update_contents(operation_list,update_list, position_list)
+#     # server.close_doc(doc)
--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@ python3 -m venv myenv
 source myenv/bin/activate
 pip install pycrdt
 pip install fastapi
+pip install uvicorn
