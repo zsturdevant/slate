45005fccb8a1b9e8885319a12ec520f4c95a6332
Douglas Pineda Gutierrez <djp5@williams.edu>
2024-12-01 23:18:52 -0500

    ui tweaks (dark mode mainly)

--- nextjs-slate/src/components/TextArea/index.tsx
+++ nextjs-slate/src/components/TextArea/index.tsx
@@ -22,16 +22,16 @@ export default function TextArea() {
     // }, []);
     
     return (
-        <div className="flex-grow flex justify-center items-center px-60">
+        <div className="flex-grow flex justify-center items-center px-60 bg-[#F9FBFD] dark:bg-[#1E202F]">
         {/* TODO: Dynamically resize depending on how much text the user has already written. This will be non paginated for now */}
         {/* bg-neutral-900 */}
         {/* This doesn't behave as nicely as I want it to. I want it to stay the same size until there is no space left on the sides
             in which case I want it to shrink */}
             <textarea ref={yuh} 
-                    className="bg-white text-red-500 placeholder:text-[#5A5A5A]
-                               flex-initial flex-shrink-0 resize-none outline-none
+                    className="bg-[#ffffff] dark:bg-[#222435] text-red-500 placeholder:text-[#5A5A5A] dark:placeholder:text-[#b5c1ec] dark:text-[#CAD3F2]
+                               flex-initial flex-shrink-0 resize-none outline-none border-[#eeeeee] border-2 dark:border-[#8FADF3] dark:border-b-transparent border-b-transparent
                                w-8/12 min-w-[500px] max-w-[650px] h-full mx-auto
-                               px-8 py-4 rounded-t-md"
+                               px-8 py-4 rounded-t-md focus:placeholder-transparent"
                     placeholder="Hello:) Type here"
                     value={text}
                     onChange={handleChange}/>
