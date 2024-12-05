9e8f7490171050dd34ccc45c14620ad333a36de2
Stella Oh <50852653+Stella-J-Oh@users.noreply.github.com>
2024-12-05 12:11:19 -0500

    yjs config

--- yjs.config.js
+++ yjs.config.js
@@ -2,6 +2,6 @@ import * as Y from 'yjs'
 import { WebsocketProvider } from 'y-websocket'
 
 const ydoc = new Y.Doc()
-const provider = new WebsocketProvider('wss://your-websocket-server.com', 'your-room-name', ydoc)
+const provider = new WebsocketProvider('wss://your-websocket-server.com', 'slate', ydoc)
 
 export { ydoc, provider }
