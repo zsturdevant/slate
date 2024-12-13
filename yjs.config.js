cbca2eadecec874ce99eb9c169080d84d49fa4bf
Stella Oh <50852653+Stella-J-Oh@users.noreply.github.com>
2024-12-12 21:28:05 -0600

    some clenaup

--- yjs.config.js
+++
@@ -1,7 +0,0 @@
-import * as Y from 'yjs'
-import { WebsocketProvider } from 'y-websocket'
-
-const ydoc = new Y.Doc()
-const provider = new WebsocketProvider('wss://your-websocket-server.com', 'slate', ydoc)
-
-export { ydoc, provider }
